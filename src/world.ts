c3e5bd8227a16e46d2634e60c930e04686cdbea2
satou-yosiyuki <stuysyk1027@icloud.com>
2022-04-09 19:27:28 +0900

    pretter

--- src/world.ts
+++ src/world.ts
@@ -3,9 +3,18 @@ export default class World {
   constructor(message: string) {
     this.message = message;
   }
-  public sayHello(elem: HTMLElement | null) {
+  public sayHello(elem: HTMLElement | null){
     if (elem) {
       elem.innerText = this.message;
     }
+
+
+
+
+
+
+
+
+
   }
 }
